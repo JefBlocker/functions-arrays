8934f3498c94c4f2988b673663dd170bbfff32ca
Jef Blocker <jefblocker@gmail.com>
2015-09-30 22:53:53 -0400

    Completed most of exercises-2.js

--- exercises-2.js
+++ exercises-2.js
@@ -2,37 +2,58 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // ---------------------
 
-function max(3, 5) {
+function max(a, b) {
+  var a = 3;
+  var b = 5;
   if ( 3 > 5 ) {
     return 3
   } else {
     return 5
   }
 };
+undefined
+max();
+5
 
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // ---------------------
 
-function maxOfThree(){
-    //...
-}
+var a = 3;
+var b = 5;
+var c = 7;
+var greater;
+var  greatest;
+function maxOfThree(a, b, c) {
+  if ( a > b ){
+    greater = a;
+  } else {
+    greater = b;
+  }
+  if ( greater > c ){
+    greatest = greater;
+  } else {
+    greatest = c;
+    return greatest;
+  }
+};
 
 // ---------------------
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // ---------------------
 
 function isVowel(char){
-    //...
+    return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || false;
 }
+
 
 // ---------------------
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
 function rovarspraket(phrase){
-    //...
+    
 }
 
 
@@ -41,7 +62,10 @@ function rovarspraket(phrase){
 // ---------------------
 
 function reverse(){
-    //...
+
+var bands = ['ABC', 'Bananarama', 'Culture Club', 'Duran Duran', 'Eurythmics']
+bands.reverse();
+["Eurythmics", "Duran Duran", "Culture Club", "Bananarama", "ABC"]
 }
 
 // ---------------------
@@ -49,8 +73,19 @@ function reverse(){
 // ---------------------
 
 function findLongestWord(words){
-    //...
+  var str = string.split('');
+  var longest = 0;
+  var word = null;
+  str.forEach(function(str) {
+    if (longest < str.length) {
+      longest = str.length;
+      word = str;
+    }
+  });
+  return word;
 }
+console.log(longestWord('chocolate', 'cheese', 'chicken')); 
+
 
 // ---------------------
 // Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
@@ -65,5 +100,13 @@ function filterLongWords(words, i){
 // ---------------------
 
 function charFreq(string){
-    //...
+  var list = {abbabcbdbabdbdbabababcbcbab"};
+  var length = string.length;
+  for (var i = 0; i < length; i++) {
+    if (string.charAt(i) in list)
+      list[string.charAt(i)] += +1;
+    else
+      list[string.charAt(i)] = 1;
+  }
+  return list;
 }
